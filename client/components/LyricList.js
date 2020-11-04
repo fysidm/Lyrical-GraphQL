547f0a3253d1c44b12b85757c9da47c6de757992
GeorgeWu <fysidm@gmail.com>
2020-11-04 15:48:26 +0800

    Add LyricList

---
+++ client/components/LyricList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class LyricList extends Component {
+  renderList() {
+    return this.props.lyrics.map(({ id, content }) => {
+      return (
+        <li key={id} className="collection-item">
+          {content}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="collection">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+export default LyricList;
